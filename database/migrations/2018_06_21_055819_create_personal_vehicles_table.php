62c076cdcd5fc729262a15d945a3cf6dda8b0d9c
Prem Singh <premsingh57@gmail.com>
2018-06-21 12:37:53 +0545

    personal vehicle added

---
+++ database/migrations/2018_06_21_055819_create_personal_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('staff_vehicle_id')->unsiged();
+            $table->string('brand');
+            $table->string('number');
+            $table->float('mileage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_vehicles');
+    }
+}
