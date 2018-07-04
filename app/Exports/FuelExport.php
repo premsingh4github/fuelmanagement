5a494f2f6600cad47e908c34247aafee1c32319b
harson5 <suwalharson@gmail.com>
2018-07-04 18:17:50 +0545

    report

--- app/Exports/FuelExport.php
+++ app/Exports/FuelExport.php
@@ -22,30 +22,25 @@ class FuelExport implements FromView
 //        $eng_startdate =  new nep_to_eng_date($enddate);
 //        dd($eng_startdate);
         $cal = new \NepaliCalendar();
-        $eng_startdate = $cal->nep_to_eng_date(date('Y-m-d'));
-        dd($eng_startdate);
-
-
-//        dd(request()->end_date);
-
-        $fuels = Fuel::whereBetween('date',[$startdate ,$enddate]);
-
-
+        $eng_start_date = $cal->nep_to_eng_date($startdate);
+        $eng_start_date = date('Y-m-d',strtotime($eng_start_date));
+        $eng_end_date = date('Y-m-d',strtotime($cal->nep_to_eng_date($enddate)));
+        $fuels = Fuel::whereBetween('created_at', array($eng_start_date.' 00:00:00',$eng_end_date.' 23:59:59'));
         if (\request('staff_id')){
-            $fuels =Fuel::where('staff_id',\request('staff_id'));
+            $fuels->where('staff_id',\request('staff_id'));
 
         }
         if (\request('mode')){
-            $fuels = Fuel::where('mode', \request('mode'));
+            $$fuels->where('mode', \request('mode'));
         }
         if (\request('petrolpump_name')){
-            $fuels= Fuel::where('petrolpump_id',\request('petrolpump_name'));
+            $fuels->where('petrolpump_id',\request('petrolpump_name'));
         }
         if (\request('receiver_id')){
-            $fuels = Fuel::where('receiver_id',\request('receiver_id'));
+            $fuels->where('receiver_id',\request('receiver_id'));
         }
 
-        $fuels =$fuels->get();
+        $fuels = $fuels->get();
 
 
         return view('admin.report.fuel', [
